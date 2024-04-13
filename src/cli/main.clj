131f0bdd178a7eb4bd9d162912dc5324437f6b29
Gert Goet <gert@thinkcreate.dk>
2024-04-13 11:15:36 +0200

    Build it

--- src/cli/main.clj
+++ src/cli/main.clj
@@ -1,5 +1,19 @@
 (ns cli.main
-  (:gen-class))
+  (:gen-class)
+  (:require [next.jdbc :as jdbc])
+  #_(:import (org.sqlite JDBC)))
+
+(def ds "jdbc:sqlite::memory:")
+
+(defn test-connection! []
+  (jdbc/execute! ds ["select datetime('now') as now"]))
 
 (defn -main [& args]
-  (prn :args args))
+  (prn (test-connection!)))
+
+(comment
+
+
+  (prn :foo)
+
+  #_:end)
